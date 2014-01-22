6d6157460a482854d34471b903b09f3aa85b4c5e
Ya-Lin Huang <yalinh@yelp.com>
2014-01-22 13:46:53 -0800

    comprehensive tests

--- my_circular_container_iterator_test.cpp
+++ my_circular_container_iterator_test.cpp
@@ -1,26 +1,119 @@
 #include <vector>
 using std::vector;
-
 #include <iostream>
 using std::cout;
 using std::endl;
 
+#include <assert.h>
+
 #include "my_circular_container_iterator.h"
 
+void unit_test_size_1(void)
+{
+	vector<int> v({1});
+	MyCircularContainerIterator<vector<int>, vector<int>::iterator> it(v, v.begin());
 
-int main(int argc, char **argv)
+    // ++it
+	for(auto i=0; i<5; ++i)
+        assert(*(++it)==1);
+
+    // it++
+    for(auto i=0; i<5; ++i)
+        assert(*(it++)==1);
+
+    // --it
+    for(auto i=0; i<5; ++i)
+        assert(*(--it)==1);
+
+    // it--
+    for(auto i=0; i<5; ++i)
+        assert(*(it--)==1);
+
+    // it+
+	for(auto i=0; i<5; ++i)
+        assert(*(it+i)==1);
+
+    // it-
+    for(auto i=0; i<5; ++i)
+        assert(*(it-i)==1);
+
+    // it+=
+    for(auto i=0; i<5; ++i)
+        assert(*(it+=i)==1);
+
+    // it-=
+    for(auto i=0; i<5; ++i)
+        assert(*(it-=i)==1);
+}
+
+void unit_test_size_5(void)
 {
 	vector<int> v({1, 2, 3, 4, 5});
 	MyCircularContainerIterator<vector<int>, vector<int>::iterator> vIt(v, v.begin());
+    MyCircularContainerIterator<vector<int>, vector<int>::iterator> it(vIt);
+    vector<int> output;
 
-	for(auto i=0; i<6; ++i, ++vIt)
-		cout << *vIt << ", ";
-	cout << endl; // "1, 2, 3, 4, 5, 1, "
-	for(auto i=0; i<6; ++i, --vIt)
-		cout << *vIt << ", ";
-	cout << endl; // "2, 1, 5, 4, 3, 2, "
-	cout << *vIt << *(vIt+3) << *(vIt+5) << *(vIt+7) << endl; // "1413"
-	cout << *vIt << *(vIt-3) << *(vIt-5) << *(vIt-7) << endl; // "1314"
+    // ++it
+	for(auto i=0; i<7; ++i)
+        output.push_back(*(++it));
+    assert(output==vector<int>({2, 3, 4, 5, 1, 2, 3}));
+    it = vIt;
+    output.clear();
+
+    // it++
+	for(auto i=0; i<7; ++i)
+        output.push_back(*(it++));
+    assert(output==vector<int>({1, 2, 3, 4, 5, 1, 2}));
+    it = vIt;
+    output.clear();
+
+    // --it
+	for(auto i=0; i<7; ++i)
+        output.push_back(*(--it));
+    assert(output==vector<int>({5, 4, 3, 2, 1, 5, 4}));
+    it = vIt;
+    output.clear();
+
+    // it--
+	for(auto i=0; i<7; ++i)
+        output.push_back(*(it--));
+    assert(output==vector<int>({1, 5, 4, 3, 2, 1, 5}));
+    it = vIt;
+    output.clear();
+
+    // it+
+	for(auto i=0; i<7; ++i)
+        output.push_back(*(it+i));
+    assert(output==vector<int>({1, 2, 3, 4, 5, 1, 2}));
+    it = vIt;
+    output.clear();
+
+    // it-
+	for(auto i=0; i<7; ++i)
+        output.push_back(*(it-i));
+    assert(output==vector<int>({1, 5, 4, 3, 2, 1, 5}));
+    it = vIt;
+    output.clear();
+
+    // it+=
+	for(auto i=0; i<7; ++i)
+        output.push_back(*(it+=i));
+    assert(output==vector<int>({1, 2, 4, 2, 1, 1, 2}));
+    it = vIt;
+    output.clear();
+
+    // it-=
+	for(auto i=0; i<7; ++i)
+        output.push_back(*(it-=i));
+    assert(output==vector<int>({1, 5, 3, 5, 1, 1, 5}));
+    it = vIt;
+    output.clear();
+}
+
+int main(int argc, char **argv)
+{
+    unit_test_size_1();
+    unit_test_size_5();
 
 	return 0;
 }
