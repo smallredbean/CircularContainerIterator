6d68c0e239997b6b03911e7d1067081fc1d07847
Beany Huang <huangyl@gmail.com>
2014-01-22 22:03:10 -0800

    space to tab

--- my_circular_container_iterator_test.cpp
+++ my_circular_container_iterator_test.cpp
@@ -13,101 +13,101 @@ void unit_test_size_1(void)
 	vector<int> v({1});
 	MyCircularContainerIterator<vector<int>, vector<int>::iterator> it(v, v.begin());
 
-    // ++it
+	// ++it
 	for(auto i=0; i<5; ++i)
-        assert(*(++it)==1);
+		assert(*(++it)==1);
 
-    // it++
-    for(auto i=0; i<5; ++i)
-        assert(*(it++)==1);
-
-    // --it
-    for(auto i=0; i<5; ++i)
-        assert(*(--it)==1);
-
-    // it--
-    for(auto i=0; i<5; ++i)
-        assert(*(it--)==1);
-
-    // it+
+	// it++
 	for(auto i=0; i<5; ++i)
-        assert(*(it+i)==1);
+		assert(*(it++)==1);
 
-    // it-
-    for(auto i=0; i<5; ++i)
-        assert(*(it-i)==1);
+	// --it
+	for(auto i=0; i<5; ++i)
+		assert(*(--it)==1);
 
-    // it+=
-    for(auto i=0; i<5; ++i)
-        assert(*(it+=i)==1);
+	// it--
+	for(auto i=0; i<5; ++i)
+		assert(*(it--)==1);
 
-    // it-=
-    for(auto i=0; i<5; ++i)
-        assert(*(it-=i)==1);
+	// it+
+	for(auto i=0; i<5; ++i)
+		assert(*(it+i)==1);
+
+	// it-
+	for(auto i=0; i<5; ++i)
+		assert(*(it-i)==1);
+
+	// it+=
+	for(auto i=0; i<5; ++i)
+		assert(*(it+=i)==1);
+
+	// it-=
+	for(auto i=0; i<5; ++i)
+		assert(*(it-=i)==1);
 }
 
 void unit_test_size_5(void)
 {
 	vector<int> v({1, 2, 3, 4, 5});
 	MyCircularContainerIterator<vector<int>, vector<int>::iterator> vIt(v, v.begin());
-    MyCircularContainerIterator<vector<int>, vector<int>::iterator> it(vIt);
-    vector<int> output;
+	MyCircularContainerIterator<vector<int>, vector<int>::iterator> it(vIt);
+	vector<int> output;
 
-    // ++it
+	// ++it
 	for(auto i=0; i<7; ++i)
-        output.push_back(*(++it));
-    assert(output==vector<int>({2, 3, 4, 5, 1, 2, 3}));
-    it = vIt;
-    output.clear();
+		output.push_back(*(++it));
+	assert(output==vector<int>({2, 3, 4, 5, 1, 2, 3}));
+	it = vIt;
+	output.clear();
 
-    // it++
+	// it++
 	for(auto i=0; i<7; ++i)
-        output.push_back(*(it++));
-    assert(output==vector<int>({1, 2, 3, 4, 5, 1, 2}));
-    it = vIt;
-    output.clear();
+		output.push_back(*(it++));
+	assert(output==vector<int>({1, 2, 3, 4, 5, 1, 2}));
+	it = vIt;
+	output.clear();
 
-    // --it
+	// --it
 	for(auto i=0; i<7; ++i)
-        output.push_back(*(--it));
-    assert(output==vector<int>({5, 4, 3, 2, 1, 5, 4}));
-    it = vIt;
-    output.clear();
+		output.push_back(*(--it));
+	assert(output==vector<int>({5, 4, 3, 2, 1, 5, 4}));
+	it = vIt;
+	output.clear();
 
-    // it--
+	// it--
 	for(auto i=0; i<7; ++i)
-        output.push_back(*(it--));
-    assert(output==vector<int>({1, 5, 4, 3, 2, 1, 5}));
-    it = vIt;
-    output.clear();
+		output.push_back(*(it--));
+	assert(output==vector<int>({1, 5, 4, 3, 2, 1, 5}));
+	it = vIt;
+	output.clear();
 
-    // it+
+	// it+
 	for(auto i=0; i<7; ++i)
-        output.push_back(*(it+i));
-    assert(output==vector<int>({1, 2, 3, 4, 5, 1, 2}));
-    it = vIt;
-    output.clear();
+		output.push_back(*(it+i));
+	assert(output==vector<int>({1, 2, 3, 4, 5, 1, 2}));
+	it = vIt;
+	output.clear();
 
-    // it-
+	// it-
 	for(auto i=0; i<7; ++i)
-        output.push_back(*(it-i));
-    assert(output==vector<int>({1, 5, 4, 3, 2, 1, 5}));
-    it = vIt;
-    output.clear();
+		output.push_back(*(it-i));
+	assert(output==vector<int>({1, 5, 4, 3, 2, 1, 5}));
+	it = vIt;
+	output.clear();
 
-    // it+=
+	// it+=
 	for(auto i=0; i<7; ++i)
-        output.push_back(*(it+=i));
-    assert(output==vector<int>({1, 2, 4, 2, 1, 1, 2}));
-    it = vIt;
-    output.clear();
+		output.push_back(*(it+=i));
+	assert(output==vector<int>({1, 2, 4, 2, 1, 1, 2}));
+	it = vIt;
+	output.clear();
 
-    // it-=
+	// it-=
 	for(auto i=0; i<7; ++i)
-        output.push_back(*(it-=i));
-    assert(output==vector<int>({1, 5, 3, 5, 1, 1, 5}));
-    it = vIt;
-    output.clear();
+		output.push_back(*(it-=i));
+	assert(output==vector<int>({1, 5, 3, 5, 1, 1, 5}));
+	it = vIt;
+	output.clear();
 }
 
 void unit_test_iterator_to_index(void)
@@ -121,8 +121,8 @@ void unit_test_iterator_to_index(void)
 
 int main(int argc, char **argv)
 {
-    unit_test_size_1();
-    unit_test_size_5();
+	unit_test_size_1();
+	unit_test_size_5();
 	unit_test_iterator_to_index();
 
 	return 0;
